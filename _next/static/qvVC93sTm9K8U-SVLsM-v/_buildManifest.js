95b2b5a2a3cee9660f4e532a9a077282ba9123b9
MarcosRigal <riveragavilanmarcos@gmail.com>
2022-03-06 20:50:40 +0000

    Deploying to gh-pages from @ UCOcamp/frontend@90ee6d6b057cb0e4be6e81c88a8eae4748f21d5e 🚀

--- _next/static/qvVC93sTm9K8U-SVLsM-v/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-b4d48420f210c0b5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
