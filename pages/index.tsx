fa71badd65a5eb6bce078185e33a90fa8796aa60
MarcosRigal <riveragavilanmarcos@gmail.com>
2022-03-06 17:33:11 +0100

    feat: :sparkles: CI

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">UCOCAMP</a>
+          Welcome to <a href="https://nextjs.org">¿ESTO FUNCIONA?</a>
         </h1>
 
         <p className={styles.description}>
