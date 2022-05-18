5b423df801c494630c80970857e903bfb735577e
Marcos Rivera Gavilán <riveragavilanmarcos@gmail.com>
2022-05-18 20:16:47 +0200

    Done Lesson Page

--- pages/lesson.tsx
+++ pages/lesson.tsx
@@ -1,7 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { Navbar } from "../components/navbar/navbar";
+import { Footer } from "../components/footer/footer";
 
 const Home: NextPage = () => {
   return (
@@ -11,17 +12,194 @@ const Home: NextPage = () => {
         <meta name="description" content="UCOCamp landing page" />
         <link rel="icon" href="/logo.png" />
       </Head>
+      <Navbar />
       <main>
-      <h1>Lección</h1>
-      <button className='loginButton'><Link href="/homepage">&nbsp;&nbsp;&nbsp;Mis cursos&nbsp;&nbsp;&nbsp;</Link></button>
-      <button className='loginButton'><Link href="/course">&nbsp;&nbsp;&nbsp;Volver al curso&nbsp;&nbsp;&nbsp;</Link></button>
-      <button className='loginButton'><Link href="/profile">&nbsp;&nbsp;&nbsp;Perfil&nbsp;&nbsp;&nbsp;</Link></button>
-      <button className='loginButton'><Link href="/">&nbsp;&nbsp;&nbsp;Salir&nbsp;&nbsp;&nbsp;</Link></button>
-      <button className='loginButton'><Link href="/lesson">&nbsp;&nbsp;&nbsp;Siguiente lección&nbsp;&nbsp;&nbsp;</Link></button>
-      <button className='loginButton'><Link href="/lesson">&nbsp;&nbsp;&nbsp;Lección anterior&nbsp;&nbsp;&nbsp;</Link></button>
+        <br></br>
+        <h1>Cursos > Lecciones</h1>
+        <br></br>
+        <section id="Speakers" className="Section Speakers">
+          <div className="grid">
+            <div className="speakerCard">
+              <div className="Photo">
+                <img src="portada.jpg" alt="Foto de Eduardo Sánchez" />
+              </div>
+              <div className="Label">
+                <h3>Lección 1</h3>
+                <p>Descripción de la lección</p>
+                <div className="RRSS">
+                  <ul>
+                    <li>
+                      <a
+                        href="https://twitter.com/eduSatoe"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-twitter"
+                      ></a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.linkedin.com/in/eduardosancheztoril/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-linkedin"
+                      ></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="speakerCard">
+              <div className="Photo">
+                <img src="portada.jpg" alt="Foto de Paula García" />
+              </div>
+              <div className="Label">
+                <h3>Lección 2</h3>
+                <p>Descripción de la lección</p>
+                <div className="RRSS">
+                  <ul>
+                    <li>
+                      <a
+                        href="https://twitter.com/paula_g_phys"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-twitter"
+                      ></a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.linkedin.com/in/paula-garc%C3%ADa-molina-538b86199/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-linkedin"
+                      ></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="speakerCard">
+              <div className="Photo">
+                <img src="portada.jpg" alt="Foto de Ines Huertas" />
+              </div>
+              <div className="Label">
+                <h3>Lección 3</h3>
+                <p>Descripción de la lección</p>
+                <div className="RRSS">
+                  <ul>
+                    <li>
+                      <a
+                        href="https://twitter.com/quierodata"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-twitter"
+                      ></a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.linkedin.com/in/ineshuertas/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-linkedin"
+                      ></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="speakerCard">
+              <div className="Photo">
+                <img src="portada.jpg" alt="Foto de David Bonilla" />
+              </div>
+              <div className="Label">
+                <h3>Lección 4</h3>
+                <p>Descripción de la lección</p>
+                <div className="RRSS">
+                  <ul>
+                    <li>
+                      <a
+                        href="https://twitter.com/david_bonilla"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-twitter"
+                      ></a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.linkedin.com/in/dbonillaf/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-linkedin"
+                      ></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="speakerCard">
+              <div className="Photo">
+                <img src="portada.jpg" alt="Foto de Carmen Ansio" />
+              </div>
+              <div className="Label">
+                <h3>Lección 5</h3>
+                <p>Descripción de la lección</p>
+                <div className="RRSS">
+                  <ul>
+                    <li>
+                      <a
+                        href="https://twitter.com/carmenansio"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-twitter"
+                      ></a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.linkedin.com/in/carmenansio/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-linkedin"
+                      ></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="speakerCard">
+              <div className="Photo">
+                <img src="portada.jpg" alt="Foto de Carlos Buenosvinos" />
+              </div>
+              <div className="Label">
+                <h3>Lección 6</h3>
+                <p>Descripción de la lección</p>
+                <div className="RRSS">
+                  <ul>
+                    <li>
+                      <a
+                        href="https://twitter.com/buenosvinos"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-twitter"
+                      ></a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.linkedin.com/in/carlosbuenosvinos/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="fa fa-linkedin"
+                      ></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
+      <br></br>
+      <Footer/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
